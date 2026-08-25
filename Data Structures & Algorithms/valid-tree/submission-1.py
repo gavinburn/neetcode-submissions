c062322f4198ddf689f5152cc1c2270fb7c35d49
gavinburn <65696841+gavinburn@users.noreply.github.com>
2026-08-25 15:42:14 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        visited = set()
+
+        for item in edges:
+            node = item[0]
+            neighbour = item[1]
+            adjList[node].append(neighbour)
+            adjList[neighbour].append(node)
+
+        def recurse(node, previous):
+            if node in visited:
+                return False
+
+            visited.add(node)
+                
+            for neighbour in adjList[node]:
+                if neighbour is not previous:
+                    if recurse(neighbour, node) is False: return False
+
+            return True
+
+        for item in visited:
+            print(item)
+
+        if recurse(0, None) and len(visited) == n: return True
+        else: return False        
