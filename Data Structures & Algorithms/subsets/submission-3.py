248ba91ce79fad6998b9a776f267c84bdab753a1
gavinburn <65696841+gavinburn@users.noreply.github.com>
2026-09-25 00:02:55 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        output = []
+        curr = []
+
+        def recurse(i):
+
+            if i >= len(nums):
+                item = curr.copy()
+                output.append(item)
+                return
+
+            curr.append(nums[i])
+            recurse(i+1)
+
+            curr.pop()
+            recurse(i+1)
+
+
+        recurse(0)
+        return output
+
+
+
+            
